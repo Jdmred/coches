233171d8a52eb4982f93adc3190a391b740e6431
Jdmred <105762703+Jdmred@users.noreply.github.com>
2023-06-07 07:36:07 -0500

    commit3

---
+++ src/models/coche.ts
@@ -0,0 +1,57 @@
+import { Model, DataTypes } from "sequelize";
+import { database } from "../database/db";
+
+import { Venta } from "./venta";
+import { Cliente } from "./cliente";
+
+export class Coche extends Model {
+    public modelo!: string;
+    public color!: string;
+    public pvc!: number; 
+}
+
+export interface CocheI{
+    modelo: string;
+    color: string;
+    pvc: number;
+}
+
+Venta.init(
+    {
+        modelo: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        color: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        pvc: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+            unique: true
+        },
+        modeloId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+            //refereces:{
+              //  model: Marca,
+              //  key:'id',
+           // }
+            
+        },
+        clienteId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Cliente,
+                key: 'id',
+            }
+        }
+    },
+    {
+        tableName: "coches",
+        sequelize: database,
+        timestamps: true 
+    }
+);
